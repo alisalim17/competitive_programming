ef4d105f42f5234b7ce1bb3f4c44d3bce24c0cdf
alisalim17 <alisalimli1899@gmail.com>
2022-02-21 16:42:44 +0400

    A. Young Physicist

--- codeforces/main.cpp
+++ codeforces/main.cpp
@@ -5,6 +5,24 @@ using namespace std;
 
 int main()
 {
+    int xs = 0, ys = 0, zs = 0, t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y, z;
+        cin >> x >> y >> z;
+        xs += x;
+        ys += y;
+        zs += z;
+    }
+    if (xs == 0 && ys == 0 && zs == 0)
+    {
+        cout << "YES";
+    }
+    else
+    {
+        cout << "NO";
+    }
 
     return 0;
 }
